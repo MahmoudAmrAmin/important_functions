c6e4ed4ff9cfa49000c4805bc7bf66899561ee99
Mahmoud amr <113695259+mahmod5000@users.noreply.github.com>
2023-09-30 16:56:08 +0300

    first commit

---
+++ ListNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mahmod on 9/14/2023.
+//
+
+#include "ListNode.h"
---
+++ ListNode.h
@@ -0,0 +1,16 @@
+//
+// Created by mahmod on 9/14/2023.
+//
+
+#ifndef CLION2_LISTNODE_H
+#define CLION2_LISTNODE_H
+
+
+
+class ListNode {
+
+};
+
+
+
+#endif //CLION2_LISTNODE_H
